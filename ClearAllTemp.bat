cc4062b92de42afb2624ca5524f655c25b4a7fc1
Mohamed Kassoum <82602657+mkassoum@users.noreply.github.com>
2023-03-23 20:33:24 +0100

    Update ClearAllTemp.bat

--- ClearAllTemp.bat
+++ ClearAllTemp.bat
@@ -1,9 +1,23 @@
-@ECHO OFF
-
-del /s /f /q %userprofile%\Recent\*.*
-
-del /s /f /q C:\Windows\Prefetch\*.*
-
-del /s /f /q C:\Windows\Temp\*.*
-
-del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+@echo off
+echo -------------------------------------------------
+echo Say Thanks to 3mo Kassoum Abu Al Naar
+PAUSE
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+cls
+RD /S /Q %temp%
+MKDIR %temp%
+takeown /f "%temp%" /r /d y
+takeown /f "C:\Windows\Temp" /r /d y
+RD /S /Q C:\Windows\Temp
+MKDIR C:\Windows\Temp
+takeown /f "C:\Windows\Temp" /r /d y
+takeown /f %temp% /r /d y
+C:\windows\SYSTEM32\cleanmgr.exe /dC
